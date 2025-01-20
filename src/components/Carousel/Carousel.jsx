161a695f285d2e6618082f2902f40f7bbd03fd7d
Gastón <gaston.garcia.bauer89@gmail.com>
2025-01-20 18:04:06 -0300

    agrega imágen al carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,7 @@
 
 import carousel1 from '../../assets/img/carousel-1.jpg'
 import carousel2 from '../../assets/img/carousel-2.jpg'
+import carousel3 from '../../assets/img/carousel-3.jpg'
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -24,8 +25,13 @@ const ImageSlider = () => {
       <div>
         <img src={carousel1} alt="First slide" className='img-carousel'/>
       </div>
+      
       <div>
         <img src={carousel2} alt="Second slide" className='img-carousel'/>
+      </div>
+      
+      <div>
+        <img src={carousel3} alt="Thirth slide" className='img-carousel'/>
       </div>
     </Slider>
     </div>
