7ed37c6f3345afcd8ada2991f7e24d09340a107d
Gastón <gaston.garcia.bauer89@gmail.com>
2025-01-20 15:58:38 -0300

    modifica sliders

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,6 @@
 
 import carousel1 from '../../assets/img/carousel-1.jpg'
 import carousel2 from '../../assets/img/carousel-2.jpg'
-import carousel3 from '../../assets/img/carousel-3.jpg'
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -27,9 +26,6 @@ const ImageSlider = () => {
       </div>
       <div>
         <img src={carousel2} alt="Second slide" className='img-carousel'/>
-      </div>
-      <div>
-        <img src={carousel3} alt="Third slide" className='img-carousel'/>
       </div>
     </Slider>
     </div>
